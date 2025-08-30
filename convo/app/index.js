0abf83d39f195935403a52794db9f1e21e110fbd
Supun Paniyanduwage <pdscharuka@gmail.com>
2025-08-31 00:59:00 +0530

    Setup the Nativewind

---
+++ convo/app/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <View style={{paddingTop: 40}}>
+      <Text>Home</Text>
+    </View>
+  )
+}
